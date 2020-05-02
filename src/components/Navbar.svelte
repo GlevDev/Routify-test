8bd2e8bc79ff0d55412cdef225d40435ac0e1ff4
GlevDev <garth@glev.fr>
2020-05-02 23:30:15 +0200

    first commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { url, isActive } from "@sveltech/routify";
+  const links = [
+    { path: "/index", name: "Home" },
+    { path: "/about", name: "About" },
+    { path: "/articles", name: "Articles" }
+  ];
+</script>
+
+<style>
+  .navlink {
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    margin: 1rem;
+  }
+
+  .active {
+    background: rgba(0,0,0,0.3);
+  }
+
+  .navlink div {
+    margin-right: 1rem;
+  }
+
+  .navlink div a {
+    padding: 5px;
+  }
+</style>
+
+<div class="navlink">
+  {#each links as link}
+    <div>
+      <a href={$url(link.path)} class:active={$isActive(link.path)}>{link.name}</a>
+    </div>
+  {/each}
+</div>
